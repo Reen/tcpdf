1f2983b63375af335f0dc8c58d9f6a88812f2a8f
René Haber <rene@habr.de>
2009-04-17 16:15:27 +0200

    tcpdf-4.6.000

--- examples/example_051.php
+++ examples/example_051.php
@@ -1,11 +1,11 @@
 <?php
 //============================================================+
-// File name   : example_042.php
-// Begin       : 2008-12-23
+// File name   : example_051.php
+// Begin       : 2009-04-16
 // Last Update : 2009-04-16
 // 
-// Description : Example 042 for TCPDF class
-//               Test Image with alpha channel
+// Description : Example 051 for TCPDF class
+//               Full page background
 // 
 // Author: Nicola Asuni
 // 
@@ -22,41 +22,54 @@
 /**
  * Creates an example PDF TEST document using TCPDF
  * @package com.tecnick.tcpdf
- * @abstract TCPDF - Example: Test Image with alpha channel
+ * @abstract TCPDF - Example: Full page background
  * @author Nicola Asuni
  * @copyright 2004-2009 Nicola Asuni - Tecnick.com S.r.l (www.tecnick.com) Via Della Pace, 11 - 09044 - Quartucciu (CA) - ITALY - www.tecnick.com - info@tecnick.com
  * @link http://tcpdf.org
  * @license http://www.gnu.org/copyleft/lesser.html LGPL
- * @since 2008-12-23
+ * @since 2009-04-16
  */
 
 require_once('../config/lang/eng.php');
 require_once('../tcpdf.php');
 
+
+// Extend the TCPDF class to create custom Header and Footer
+class MYPDF extends TCPDF {
+	//Page header
+	public function Header() {
+		// Full background image
+		$auto_page_break = $this->AutoPageBreak;
+		$this->SetAutoPageBreak(false, 0);
+		$img_file = K_PATH_IMAGES.'image_demo.jpg';
+		$this->Image($img_file, $x=0, $y=0, $w=210, $h=297, $type='', $link='', $align='', $resize=false, $dpi=300, $palign='', $ismask=false, $imgmask=false, $border=0);
+		$this->SetAutoPageBreak($auto_page_break);
+	}
+}
+
 // create new PDF document
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false); 
+$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('TCPDF Example 042');
+$pdf->SetTitle('TCPDF Example 051');
 $pdf->SetSubject('TCPDF Tutorial');
 $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
 
-// set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
-
 // set header and footer fonts
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
 // set default monospaced font
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
 //set margins
 $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+$pdf->SetHeaderMargin(0);
+$pdf->SetFooterMargin(0);
+
+// remove default footer
+$pdf->setPrintFooter(false);
 
 //set auto page breaks
 $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
@@ -69,35 +82,25 @@ $pdf->setLanguageArray($l);
 
 // ---------------------------------------------------------
 
-// set JPEG quality
-//$pdf->setJPEGQuality(75);
-
-$pdf->SetFont('helvetica', '', 18);
+// set font
+$pdf->SetFont('times', '', 48);
 
 // add a page
 $pdf->AddPage();
 
-// create background text
-$background_text = str_repeat('TCPDF test PNG Alpha Channel ', 50);
-$pdf->MultiCell(0, 5, $background_text, 0, 'J', 0, 2, '', '', true, 0, false);
+// Pritn a text
+$pdf->writeHTML('<span style="background-color:yellow;color:blue">PAGE 1</span>', $ln=true, $fill=false, $reseth=false, $cell=false, $align='');
 
+// add a page
+$pdf->AddPage();
 
-// [A] The Image() method recognizes the alpha channel embedded on the image:
-
-$pdf->Image('../images/image_with_alpha.png', 50, 50, 100, '', '', 'http://www.tcpdf.org', '', false, 300);
-
-
-// [B] provide image + separate 8-bit mask
-
-// first embed mask image (w, h, x and y will be ignored, the image will be scaled to the target image's size)
-$mask = $pdf->Image('../images/alpha.png', 50, 140, 100, '', '', '', '', false, 300, '', true);
-// embed image, masked with previously embedded mask
-$pdf->Image('../images/img.png', 50, 140, 100, '', '', 'http://www.tcpdf.org', '', false, 300, '', false, $mask);
+// Pritn a text
+$pdf->writeHTML('<span style="background-color:yellow;color:blue">PAGE 2</span>', $ln=true, $fill=false, $reseth=false, $cell=false, $align='');
 
 // ---------------------------------------------------------
 
 //Close and output PDF document
-$pdf->Output('example_042.pdf', 'I');
+$pdf->Output('example_051.pdf', 'I');
 
 //============================================================+
 // END OF FILE                                                 
